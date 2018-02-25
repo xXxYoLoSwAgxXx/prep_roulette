4aafa72efdf2113a3de1b0c0fed9dcb051125dd3
Victor <viclee02@gmail.com>
2018-02-24 16:47:42 -1000

    adds roulette definition

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-console.log("sanity check")
+console.log("sanity check");
+
+/*From wikipedia https://en.wikipedia.org/wiki/Roulette
+Roulette is a casino game named after the French word meaning little wheel. In the game, players may choose to place bets on either a single number, various groupings of numbers, the colors red or black, whether the number is odd or even, or if the numbers are high (19–36) or low (1–18).*/
